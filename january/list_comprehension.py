c0d0a8cffc2430b79d42fe89d9d8e8309dd6da04
Maha-Purush <balajikamat.504@gmail.com>
2026-01-31 21:55:54 +0530

    regular commit

---
+++ january/list_comprehension.py
@@ -0,0 +1,13 @@
+#no_dupes = set(item.capitalize() for item in input("Enter a couple of words: ").split(" "))
+#print(no_dupes)
+
+#no_dupes = set(item.upper() for item in input("Enter a couple of words: ").split(" "))
+#print(no_dupes)
+
+
+#LIST = [item.lower() for item in input("Enter a bunch of words (separated by commas): ").split(",")]
+#print(LIST)
+
+
+#numbers = [int(num) for num in input("Enter a list of positive numbers (separated by commas): ").split(",") if int(num)>5]
+#print("Numbers greater than 5 are: ", numbers)
