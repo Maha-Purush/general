f76d0ae34dbe4187eda8f8546534c8c7d4f0f04f
Maha-Purush <balajikamat.504@gmail.com>
2026-02-06 11:08:26 +0530

    routine commit

--- february/class_example.py
+++ february/class_example.py
@@ -8,32 +8,40 @@
 #print(Student2.class_var)
 
 #------------------------------------------------------------------------
-class Animal:
-  def __init__(self, name):
-    self.name = name
-    self.is_Alive = True
+#class Animal:
+  #def __init__(self, name):
+    #self.name = name
+    #self.is_Alive = True
 
-  def eat(self):
-    print(f"{self.name} is eating.")
+  #def eat(self):
+    #print(f"{self.name} is eating.")
 
-  def sleep(self):
-    print(f"{self.name} is sleeping.")
+  #def sleep(self):
+    #print(f"{self.name} is sleeping.")
 
-class Dog(Animal):
-  pass
+#class Dog(Animal):
+  #pass
 
-class Cat(Animal):
-  pass
+#class Cat(Animal):
+  #pass
 
-class Mouse(Animal):
-  def cook(self):
-    print(f"{self.name} is cooking.")
+#class Mouse(Animal):
+  #def cook(self):
+    #print(f"{self.name} is cooking.")
   
 #dog = Dog("Shiba")
 #dog.eat()
 
-chef = Mouse("Remy")
-chef.cook()
-chef.eat()
-chef.sleep()
-print(chef.is_Alive)
+#chef = Mouse("Remy")
+#chef.cook()
+#chef.eat()
+#chef.sleep()
+#print(chef.is_Alive)
+
+#------------------------------------------------------------------------
+
+class Shape:
+  def __init__(self):
+    pass
+
+class Circle(Shape):
