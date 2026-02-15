9fc6ecce770cc2f4402fdb39f2479e0145c63487
Maha-Purush <balajikamat.504@gmail.com>
2026-02-15 21:41:10 +0530

    routine commit

--- february/exceptions.py
+++ february/exceptions.py
@@ -1,6 +1,17 @@
-number = float(input("Enter a denominator for the expression [1/number]:"))
+  # number = float(input("Enter a denominator for the expression [1/number]:"))
+  # try:
+  #   print(1/number)
+  #   number = float(input("Enter a denominator for the expression [1/number]:"))
+  # except Exception:
+  #     print("Enter a valid number.")
+
+class IDKException(Exception):
+  pass
 try:
-  print(1/number)
   number = float(input("Enter a denominator for the expression [1/number]:"))
-except Exception:
-    print("Enter a valid number.")
+  if number<=0:
+    raise IDKException
+  else:
+    print(1/number)
+except IDKException:
+  print("bruh")
