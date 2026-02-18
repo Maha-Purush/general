672208bb1ee106435027afe731dfaedb15858ffb
Maha-Purush <balajikamat.504@gmail.com>
2026-02-19 04:14:46 +0530

    asyncio.py

--- february/async_example.py
+++ february/async_example.py
@@ -1,6 +1,44 @@
-import asyncio as aio
+# import asyncio 
 
-async def main():
-    print("Start of main coroutine.")
+# async def confusement(delay):
+#   await asyncio.sleep(delay)
+#   print("i am waiting, so confused bruh..")
 
-aio.run(main())
+# async def func1(delay):
+#   print("Fetching data...")
+#   await confusement(delay)
+#   await asyncio.sleep(delay)
+#   print("Data fetched")
+#   return {"data":"Some data"}
+
+# async def mainfunc():
+#   print("Start of main coroutine: ")
+#   task = func1(10)
+#   result = await task
+#   print(f"The result is:{result}")
+#   print("c'est fini")
+
+# asyncio.run(mainfunc())
+
+################################################
+import asyncio 
+
+async def func1(delay, id):
+  print(f"Fetching data for id{id}...")
+  await asyncio.sleep(delay)
+  print("Data fetched")
+  return {f"data{id}":"Some data"}
+
+async def mainfunc():
+  print("Start of main coroutine: ")
+  task1 = func1(5, 1)
+  task2 = func1(5, 2)
+  result = await task1
+  print(f"The result is:{result}")
+
+  result = await task2
+  print(f"The result is:{result}")
+  
+  print("c'est fini")
+
+asyncio.run(mainfunc())
